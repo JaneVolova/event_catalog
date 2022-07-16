f01ed568b4ba8c8ef7d97bbd05931edd7da0d484
Jenya Volova <jane.volova.dev@gmail.com>
2022-07-16 22:06:21 +0300

    initial commit

---
+++ src/main/java/ru/volova/event_catalog/EventCatalogApplication.java
@@ -0,0 +1,13 @@
+package ru.volova.event_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/volova/event_catalog/EventCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volova.event_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
